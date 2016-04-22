66f05ea2728440f8e05d88fa3506a316c02c999a
hmoriz <dummy@example.com>
2016-04-22 15:24:33 +0900

    The First Commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
@@ -0,0 +1,30 @@
+package jp.ac.titech.itpro.sdl.greetings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    private TextView outputView;
+    private EditText inputName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        outputView = (TextView)findViewById(R.id.output_view);
+        inputName = (EditText)findViewById(R.id.input_name);
+    }
+
+    public void greets(View v) {
+        String name = inputName.getText().toString();
+        if(name.length() > 0)
+            outputView.setText("Hello, " + name + ".\nNice to see you!");
+    }
+}
